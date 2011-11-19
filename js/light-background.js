c6e52cd2df19804a482633257c828ebfdbdb9e2b
Fabien Deshayes <fabien.deshayes@bluecallisto.com>
2011-11-19 18:54:03 +0000

    adding new background color

--- js/light-background.js
+++ js/light-background.js
@@ -1,41 +1,50 @@
 var canvas = document.getElementsByTagName('canvas')[0],
-    ctx = null,
-    grad = null;
+	ctx = canvas.getContext('2d'),
+    blueColors = [
+	'rgba(41, 183, 229, 0.95)',
+	'rgba(0, 186, 241, 0.8)',
+	'rgba(55, 68, 80, 0)',
+	'rgba(0, 252, 255, 0.32)'
+	],
+	orangeColors = [
+	'rgba(229, 136, 41, 0.95)',
+	'rgba(241, 120, 0, 0.8)',
+	'rgba(55, 68, 80, 0)',
+	'rgba(241, 120, 0, 0.3)'
+	];
+	
+function drawRect(fill, size)
+{
+	ctx.fillStyle = fill;
+    ctx.fillRect(0, 0, size, size);
+    ctx.save();
+}
     
-if (canvas.getContext('2d')) {
+if (ctx)
+{
 	var canvasSize = 500; // need to match the value in navigation.html
-    var centerWidth = canvasSize / 2; 
-    var centerHeight = canvasSize / 2;
+    var canvasCenter = canvasSize / 2; 
     var circleSize = canvasSize;
-    var firstColor = 'rgba(41, 183, 229, 0.95)';
-    var secondColor = 'rgba(0, 186, 241, 0.8)';
-    var thirdColor = 'rgba(55, 68, 80, 0)';
-    var fourthColor = 'rgba(0, 252, 255, 0.32)';
-    var fifthColor = 'rgba(0, 186, 241, 0.3)';
-        
-  ctx = canvas.getContext('2d');
-  // clear the screen
-  ctx.clearRect(0, 0, canvasSize, canvasSize);
-  ctx.save();
-  
-  // Create first radial gradient
-  grad = ctx.createRadialGradient(centerWidth, centerHeight, 0, centerWidth, centerHeight, circleSize); 
-  grad.addColorStop(0, firstColor);
-  grad.addColorStop(0.27, secondColor);
-  grad.addColorStop(0.9, thirdColor);
+    var colors = orangeColors; // pick the color set
 
-  // draw the first radial gradient
-  ctx.fillStyle = grad;
-  ctx.fillRect(0, 0, canvasSize, canvasSize);
-  ctx.save();
+    // clear the screen
+    ctx.clearRect(0, 0, canvasSize, canvasSize);
+    ctx.save();
   
-  // Create second radial gradient
-  grad = ctx.createRadialGradient(centerWidth, centerHeight, 0, centerWidth, centerHeight, circleSize); 
-  grad.addColorStop(0.35, fourthColor);
-  grad.addColorStop(1, thirdColor);
+    // Create first radial gradient
+    var gradient = ctx.createRadialGradient(canvasCenter, canvasCenter, 0, canvasCenter, canvasCenter, circleSize); 
+    gradient.addColorStop(0, colors[0]);
+    gradient.addColorStop(0.27, colors[1]);
+    gradient.addColorStop(0.9, colors[2]);
 
-  // draw the second radial gradient
-  ctx.fillStyle = grad;
-  ctx.fillRect(0, 0, canvasSize, canvasSize);
-  ctx.save();
+    // draw the first radial gradient
+    drawRect(gradient, canvasSize);
+  
+    // Create second radial gradient
+    gradient = ctx.createRadialGradient(canvasCenter, canvasCenter, 0, canvasCenter, canvasCenter, circleSize); 
+    gradient.addColorStop(0.35, colors[3]);
+    gradient.addColorStop(1, colors[2]);
+
+    // draw the second radial gradient
+    drawRect(gradient, canvasSize);
 }
