c785a6c171e2f86da26495ca04d58dce634a001b
Fabien Deshayes <fabien.deshayes@bluecallisto.com>
2011-11-28 23:07:11 +0000

    Made one arrow a button like, with three states (up, over, down)
    Using Kinetic 2.3.0.
    Code needs cleaning

--- js/compass.js
+++ js/compass.js
@@ -1,100 +1,148 @@
-// on startup, draw the compass in the area defined by the second canvas of the document
-// all positions are relative to the size of the canvas ; the compass will take
-// the full space available to be drawn in it.
+var docWidth = document.width;
+var docHeight = document.height;
+var width = 600;
+var height = 400;
 
-var canvas = document.getElementsByTagName('canvas')[1];
-var context = canvas.getContext('2d');
-var width = canvas.width;
-var height = canvas.height;
+var stage = new Kinetic.Stage("compass", width, height);
 
-const COMPASS_CENTER_RADIUS = canvas.width / 18;
+const COMPASS_CENTER_RADIUS = width / 18;
 const PADDING = COMPASS_CENTER_RADIUS / 4;
 const ARROWS_PADDING = COMPASS_CENTER_RADIUS + PADDING;
 
-drawLeftArrow();
-drawRightArrow();
-drawTopArrow();
-drawBottomArrow();
-drawCenter();
-
-function drawLeftArrow()
-{
-	prepareContextStyles();
-	startDraw();
+var leftArrow = new Kinetic.Shape(function(state){
+	var context = this.getContext();
+	
+	prepareContextStyles(context);
+	
+	if (state == "over")
+	{
+		context.fillStyle = '#cccccc';
+	}
+	else if (state == "out")
+	{
+		context.fillStyle = '#ffffff';	
+	}
+	else if (state == "up")
+	{
+		context.fillStyle = '#cccccc';	
+	}
+	else if (state == "down")
+	{
+		context.fillStyle = '#aaaaaa';	
+	}
+	startDraw(context);
 	
 	context.moveTo(0, height / 2);
 	context.lineTo(width / 2 - ARROWS_PADDING, height / 2 - COMPASS_CENTER_RADIUS);
 	context.lineTo(width / 2 - ARROWS_PADDING, height / 2 + COMPASS_CENTER_RADIUS);
 	context.lineTo(0, height / 2);
 	
-	endDraw();	
-}
+	endDraw(context);});
+leftArrow.addEventListener(
+	"mouseover",
+	function(){
+        leftArrow.draw("over");
+    });
+leftArrow.addEventListener(
+	"mouseout",
+	function(){
+        leftArrow.draw("out");
+    });
+leftArrow.addEventListener(
+	"mousedown",
+	function(){
+        leftArrow.draw("down");
+    });
+leftArrow.addEventListener(
+	"mouseup",
+	function(){
+        leftArrow.draw("up");
+    });
+ 
+/*var rightArrow = new Kinetic.Shape(drawRightArrow);
+var bottomArrow = new Kinetic.Shape(drawTopArrow);
+var upArrow = new Kinetic.Shape(drawBottomArrow);
+var compassCenter = new Kinetic.Shape(drawCenter);*/
+
+stage.add(leftArrow);
+/*stage.add(rightArrow);
+stage.add(bottomArrow);
+stage.add(upArrow);
+stage.add(compassCenter);*/
 
 function drawRightArrow()
 {
-	prepareContextStyles();
-	startDraw();
+	var context = this.getContext();
+	
+	prepareContextStyles(context);
+	startDraw(context);
 	
 	context.moveTo(width, height / 2);
 	context.lineTo(width / 2 + ARROWS_PADDING, height / 2 - COMPASS_CENTER_RADIUS);
 	context.lineTo(width / 2 + ARROWS_PADDING, height / 2 + COMPASS_CENTER_RADIUS);
 	context.lineTo(width, height / 2);
 	
-	endDraw();	
+	endDraw(context);
 }
 
 function drawTopArrow()
 {
-	prepareContextStyles();
-	startDraw();
+	var context = this.getContext();
+	
+	prepareContextStyles(context);
+	startDraw(context);
 	
 	context.moveTo(width / 2, 0);
 	context.lineTo(width / 2 + COMPASS_CENTER_RADIUS, height / 2 - ARROWS_PADDING);
 	context.lineTo(width / 2 - COMPASS_CENTER_RADIUS, height / 2 - ARROWS_PADDING);
 	context.lineTo(width / 2, 0);
 	
-	endDraw();	
+	endDraw(context);
 }
 
 function drawBottomArrow()
 {
-	prepareContextStyles();
-	startDraw();
+	var context = this.getContext();
+	
+	prepareContextStyles(context);
+	startDraw(context);
 	
 	context.moveTo(width / 2, height);
 	context.lineTo(width / 2 + COMPASS_CENTER_RADIUS, height / 2 + ARROWS_PADDING);
 	context.lineTo(width / 2 - COMPASS_CENTER_RADIUS, height / 2 + ARROWS_PADDING);
 	context.lineTo(width / 2, height);
 	
-	endDraw();	
+	endDraw(context);
 }
 
 function drawCenter()
 {
-	prepareContextStyles();
-	startDraw();
+	var context = this.getContext();
+	
+	prepareContextStyles(context);
+	startDraw(context);
 	
 	context.arc(width / 2, height / 2, COMPASS_CENTER_RADIUS, 0, 2 * Math.PI, false);
 	
-	endDraw();	
+	endDraw(context);
 }
 
 // set the style elements of the compass
-function prepareContextStyles()
+function prepareContextStyles(context)
 {
 	context.fillStyle = '#ffffff';
 	context.strokeStyle = '#000000';
 	context.lineWidth = 1;
 }
 
-function startDraw()
+function startDraw(context)
 {
 	context.beginPath();
 }
 
-function endDraw()
+function endDraw(context)
 {
+	context.closePath();
 	context.fill();
 	context.stroke();
-	context.closePath();
 }
