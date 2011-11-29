49ce6b7d8ed1425efd8655c7ec6b6235043a9281
Fabien Deshayes <fabien.deshayes@bluecallisto.com>
2011-11-29 23:06:53 +0000

    Creating interactive.js with Button class
    All elements are now buttons with 4 different states to be drawn

--- js/compass.js
+++ js/compass.js
@@ -1,148 +1,103 @@
-var docWidth = document.width;
-var docHeight = document.height;
+var docWidth = document.width - 20;
+var docHeight = document.height - 20;
 var width = 600;
 var height = 400;
 
-var stage = new Kinetic.Stage("compass", width, height);
+var stage = new Kinetic.Stage("compass", docWidth, docHeight);
 
 const COMPASS_CENTER_RADIUS = width / 18;
 const PADDING = COMPASS_CENTER_RADIUS / 4;
 const ARROWS_PADDING = COMPASS_CENTER_RADIUS + PADDING;
 
-var leftArrow = new Kinetic.Shape(function(state){
+var leftArrow = new Interactive.Button(prepare, drawLeftArrow);
+var rightArrow = new Interactive.Button(prepare, drawRightArrow);
+var topArrow = new Interactive.Button(prepare, drawTopArrow);
+var bottomArrow = new Interactive.Button(prepare, drawBottomArrow);
+var compassCenter = new Interactive.Button(prepare, drawCenter);
+
+leftArrow.shape.x = docWidth / 2 - width / 2;
+leftArrow.shape.y = docHeight / 2 - height / 2;
+rightArrow.shape.x = docWidth / 2 - width / 2;
+rightArrow.shape.y = docHeight / 2 - height / 2;
+topArrow.shape.x = docWidth / 2 - width / 2;
+topArrow.shape.y = docHeight / 2 - height / 2;
+bottomArrow.shape.x = docWidth / 2 - width / 2;
+bottomArrow.shape.y = docHeight / 2 - height / 2;
+compassCenter.shape.x = docWidth / 2 - width / 2;
+compassCenter.shape.y = docHeight / 2 - height / 2;
+
+stage.add(leftArrow.shape);
+stage.add(rightArrow.shape);
+stage.add(bottomArrow.shape);
+stage.add(topArrow.shape);
+stage.add(compassCenter.shape);
+
+function prepare(state)
+{
 	var context = this.getContext();
-	
-	prepareContextStyles(context);
-	
-	if (state == "over")
+		
+	switch (state)
 	{
-		context.fillStyle = '#cccccc';
+		case "down":
+			context.fillStyle = '#aaaaaa';
+			break;
+		case "over":
+		case "up":
+			context.fillStyle = '#cccccc';
+			break;
+		case "out":
+		default:
+			context.fillStyle = '#ffffff';
+			break;
 	}
-	else if (state == "out")
-	{
-		context.fillStyle = '#ffffff';	
-	}
-	else if (state == "up")
-	{
-		context.fillStyle = '#cccccc';	
-	}
-	else if (state == "down")
-	{
-		context.fillStyle = '#aaaaaa';	
-	}
-	startDraw(context);
-	
+		
+	context.strokeStyle = '#000000';
+	context.lineWidth = 1;
+}
+
+function drawLeftArrow(state)
+{
+	var context = this.getContext();
+		
 	context.moveTo(0, height / 2);
 	context.lineTo(width / 2 - ARROWS_PADDING, height / 2 - COMPASS_CENTER_RADIUS);
 	context.lineTo(width / 2 - ARROWS_PADDING, height / 2 + COMPASS_CENTER_RADIUS);
 	context.lineTo(0, height / 2);
-	
-	endDraw(context);});
-leftArrow.addEventListener(
-	"mouseover",
-	function(){
-        leftArrow.draw("over");
-    });
-leftArrow.addEventListener(
-	"mouseout",
-	function(){
-        leftArrow.draw("out");
-    });
-leftArrow.addEventListener(
-	"mousedown",
-	function(){
-        leftArrow.draw("down");
-    });
-leftArrow.addEventListener(
-	"mouseup",
-	function(){
-        leftArrow.draw("up");
-    });
- 
-/*var rightArrow = new Kinetic.Shape(drawRightArrow);
-var bottomArrow = new Kinetic.Shape(drawTopArrow);
-var upArrow = new Kinetic.Shape(drawBottomArrow);
-var compassCenter = new Kinetic.Shape(drawCenter);*/
+}
 
-stage.add(leftArrow);
-/*stage.add(rightArrow);
-stage.add(bottomArrow);
-stage.add(upArrow);
-stage.add(compassCenter);*/
-
-function drawRightArrow()
+function drawRightArrow(state)
 {
 	var context = this.getContext();
-	
-	prepareContextStyles(context);
-	startDraw(context);
 	
 	context.moveTo(width, height / 2);
 	context.lineTo(width / 2 + ARROWS_PADDING, height / 2 - COMPASS_CENTER_RADIUS);
 	context.lineTo(width / 2 + ARROWS_PADDING, height / 2 + COMPASS_CENTER_RADIUS);
 	context.lineTo(width, height / 2);
-	
-	endDraw(context);
 }
 
-function drawTopArrow()
+function drawTopArrow(state)
 {
 	var context = this.getContext();
-	
-	prepareContextStyles(context);
-	startDraw(context);
 	
 	context.moveTo(width / 2, 0);
 	context.lineTo(width / 2 + COMPASS_CENTER_RADIUS, height / 2 - ARROWS_PADDING);
 	context.lineTo(width / 2 - COMPASS_CENTER_RADIUS, height / 2 - ARROWS_PADDING);
 	context.lineTo(width / 2, 0);
-	
-	endDraw(context);
 }
 
-function drawBottomArrow()
+function drawBottomArrow(state)
 {
 	var context = this.getContext();
-	
-	prepareContextStyles(context);
-	startDraw(context);
 	
 	context.moveTo(width / 2, height);
 	context.lineTo(width / 2 + COMPASS_CENTER_RADIUS, height / 2 + ARROWS_PADDING);
 	context.lineTo(width / 2 - COMPASS_CENTER_RADIUS, height / 2 + ARROWS_PADDING);
 	context.lineTo(width / 2, height);
-	
-	endDraw(context);
 }
 
-function drawCenter()
+function drawCenter(state)
 {
 	var context = this.getContext();
-	
-	prepareContextStyles(context);
-	startDraw(context);
-	
+
 	context.arc(width / 2, height / 2, COMPASS_CENTER_RADIUS, 0, 2 * Math.PI, false);
-	
-	endDraw(context);
-}
-
-// set the style elements of the compass
-function prepareContextStyles(context)
-{
-	context.fillStyle = '#ffffff';
-	context.strokeStyle = '#000000';
-	context.lineWidth = 1;
-}
-
-function startDraw(context)
-{
-	context.beginPath();
-}
-
-function endDraw(context)
-{
-	context.closePath();
-	context.fill();
-	context.stroke();
 }
---
+++ js/interactive.js
@@ -0,0 +1,78 @@
+var Interactive = {};
+
+/****************************************
+ * Button
+ */
+ 
+Interactive.Button = function(prepare, drawButton){
+	this.shape = new Kinetic.Shape(this.innerDraw);
+	this.shape.prepare = prepare;
+	this.shape.drawButton = drawButton;
+	
+	var that = this;
+	
+	this.shape.addEventListener(
+		"mouseover",
+		function(event){
+			that.shape.draw("over");
+		});
+	this.shape.addEventListener(
+		"mouseout",
+		function(){
+			that.shape.draw("out");
+		});
+	this.shape.addEventListener(
+		"mousedown",
+		function(){
+			that.shape.draw("down");
+		});
+	this.shape.addEventListener(
+		"mouseup",
+		function(){
+			that.state = "up";
+			that.shape.draw("up");
+		});
+};
+
+Interactive.Button.prototype.innerDraw = function(state){
+    var context = this.getContext();
+    this.clear();
+    
+    context.save();
+    
+    if (this.prepare)
+    {
+    	this.prepare(state);
+    }
+    
+	context.beginPath();
+	
+	if (this.drawButton)
+	{
+		this.drawButton(state);
+	}
+	
+	context.closePath();
+	context.fill();
+	context.stroke();
+	
+    context.restore();
+};
+
+Interactive.Button.prototype.getContext = function(){
+    return this.shape.getContext();
+};
+
+Interactive.Button.prototype.getCanvas = function(){
+    return this.shape.getCanvas();
+};
+
+Interactive.Button.prototype.draw = function(){
+    this.shape.draw();
+};
+
+Interactive.Button.prototype.clear = function(){
+    var context = this.getContext();
+    var canvas = this.getCanvas();
+    context.clearRect(0, 0, canvas.width, canvas.height);
+};
